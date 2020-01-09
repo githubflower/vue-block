017ff9372626a390bd324c5749863f32b31f1dad
Zhang Jie <jzhang@liegroup.com>
2020-01-09 22:23:26 +0800

    vue-block init

---
+++ src/components/Test.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="formdiv">
+    <label for="inputType">赋值方式</label>
+    <select name="inputType">
+        <option value="1" selected>自定义输入</option>
+        <option value="2" >已存在的变量</option>
+    </select>
+    <input type="text" name="dataValue" id="formDivInput"/>
+    <br/>
+    <input type="button" value="确定" id="confirmBtn">
+    <input type="button" value="取消" id="cancelBtn">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      showForm: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+#formdiv{
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 10px;
+    border: 1px solid #ff0000;
+}
+#formDivInput{
+  width: 100px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Test from '@/components/Test'
+import MoveForm from '@/components/MoveForm'
+import IfForm from '@/components/IfForm'
+import SightForm from '@/components/SightForm'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
+    },
+    {
+      path: '/moveForm',
+      name: 'MoveForm',
+      component: MoveForm
+    },
+    {
+      path: '/ifForm',
+      name: 'IfForm',
+      component: IfForm
+    },
+    {
+      path: '/SightForm',
+      name: 'SightForm',
+      component: SightForm
+    }
+  ]
+})
